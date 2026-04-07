bb81f7fa286e48128a8844e613a04dc051c685f3
jvogel <joevogel501@gmail.com>
2026-04-07 16:27:14 -0700

    Fix hardcoded cache keys that would expire, now queries dynamically at runtime

--- data/create_dataset.sh
+++ data/create_dataset.sh
@@ -9,7 +9,7 @@ set -eou pipefail
 START_TIMER=$(date +%s)
 
 # Download latest PubChemLite dataset via Zenodo API (resolves latest version automatically using concept_id)
-# If the script fails, check that the concept_id is still correct by inspecting the "conceptrecid" field from https://zenodo.org/api/records/19346260
+# If the pubchemlite download fails, check that the concept_id is still correct by inspecting the "conceptrecid" field from https://zenodo.org/api/records/19346260
 CONCEPT_ID=4081056 # should remain constant
 PUBCHEMLITE_URL=$(curl -s "https://zenodo.org/api/records?q=conceptrecid:${CONCEPT_ID}&sort=mostrecent&size=1" \
   | jq -r '.hits.hits[0].files[0].links.self')
@@ -25,17 +25,29 @@ rm "pubchemlite_${PUBCHEMLITE_DATE}.csv"
 divider
 
 # Download PubChem MS Data
-# Other MS
-wget 'https://pubchem.ncbi.nlm.nih.gov/sdq/sphinxql.cgi?infmt=json&outfmt=csv&query={%22download%22:%20%22cid,inchikey,inchi,cmpdname,smiles,mf,exactmass,pclidcnt,gpidcnt%22,%22collection%22:%22compound%22,%22order%22:[%22relevancescore,desc%22],%22start%22:1,%22limit%22:10000000,%22downloadfilename%22:%22PubChem_compound_CID:_czTUKt9HuvuN0bjIOrDx7xTtU4339TJ6SF8pNlNOOzdTVwc%22,%22where%22:{%22ands%22:[{%22input%22:{%22type%22:%22netcachekey%22,%22idtype%22:%22cid%22,%22key%22:%22czTUKt9HuvuN0bjIOrDx7xTtU4339TJ6SF8pNlNOOzdTVwc%22}}]}}&showcolumndisplayname=1' -O "other-ms.csv"
+# Fetch ephemeral cache keys fresh at runtime via the PubChem classification API
+# The classification_2.fcgi endpoint returns a CacheKey for a given hierarchy node (hnid).
+# hid=72 is the PubChem Compound TOC "Mass Spectrometry" hierarchy
+PUBCHEM_CACHE_URL="https://pubchem.ncbi.nlm.nih.gov/classification_2/classification_2.fcgi?hid=72&cache_uid_type=Compound&format=json"
+pubchem_download() {
+  local hnid="$1"
+  local outfile="$2"
+  local key
+  key=$(curl -s "${PUBCHEM_CACHE_URL}&hnid=${hnid}" | jq -r '.Hierarchies.CacheKey')
+  wget "https://pubchem.ncbi.nlm.nih.gov/sdq/sphinxql.cgi?infmt=json&outfmt=csv&query={%22download%22:%20%22cid,cmpdname,inchikey,inchi,smiles,mf,exactmass,gpidcnt,pclidcnt%22,%22collection%22:%22compound%22,%22order%22:[%22relevancescore,desc%22],%22start%22:1,%22limit%22:10000000,%22where%22:{%22ands%22:[{%22input%22:{%22type%22:%22netcachekey%22,%22idtype%22:%22cid%22,%22key%22:%22${key}%22}}]}}&showcolumndisplayname=1" -O "${outfile}"
+}
+
+# Other MS (hnid=3857762)
+pubchem_download 3857762 "other-ms.csv"
 divider
-# GC-MS
-wget 'https://pubchem.ncbi.nlm.nih.gov/sdq/sphinxql.cgi?infmt=json&outfmt=csv&query={%22download%22:%20%22cid,cmpdname,inchikey,inchi,smiles,mf,exactmass,gpidcnt,pclidcnt%22,%22collection%22:%22compound%22,%22order%22:[%22relevancescore,desc%22],%22start%22:1,%22limit%22:10000000,%22downloadfilename%22:%22PubChem_compound_CID:_ImWFR2XCAH43UIhJCjHBbiRsfQxizOjxktTzvYnF4byJ3N0%22,%22where%22:{%22ands%22:[{%22input%22:{%22type%22:%22netcachekey%22,%22idtype%22:%22cid%22,%22key%22:%22ImWFR2XCAH43UIhJCjHBbiRsfQxizOjxktTzvYnF4byJ3N0%22}}]}}&showcolumndisplayname=1' -O "gc-ms.csv"
+# GC-MS (hnid=1856940)
+pubchem_download 1856940 "gc-ms.csv"
 divider
-# LC-MS
-wget 'https://pubchem.ncbi.nlm.nih.gov/sdq/sphinxql.cgi?infmt=json&outfmt=csv&query={%22download%22:%20%22cid,cmpdname,inchikey,inchi,smiles,mf,exactmass,gpidcnt,pclidcnt%22,%22collection%22:%22compound%22,%22order%22:[%22relevancescore,desc%22],%22start%22:1,%22limit%22:10000000,%22downloadfilename%22:%22PubChem_compound_CID:_4aZGhKYNw7H0n0uGyf4Coeejv8MJkItR8XSQHeplghzqfL4%22,%22where%22:{%22ands%22:[{%22input%22:{%22type%22:%22netcachekey%22,%22idtype%22:%22cid%22,%22key%22:%224aZGhKYNw7H0n0uGyf4Coeejv8MJkItR8XSQHeplghzqfL4%22}}]}}&showcolumndisplayname=1' -O lc-ms.csv
+# LC-MS (hnid=3857761)
+pubchem_download 3857761 "lc-ms.csv"
 divider
-# MS-MS
-wget 'https://pubchem.ncbi.nlm.nih.gov/sdq/sphinxql.cgi?infmt=json&outfmt=csv&query={%22download%22:%20%22cid,cmpdname,inchikey,inchi,smiles,mf,exactmass,gpidcnt,pclidcnt%22,%22collection%22:%22compound%22,%22order%22:[%22relevancescore,desc%22],%22start%22:1,%22limit%22:10000000,%22downloadfilename%22:%22PubChem_compound_CID:_mN8__d9cuuCNzjLXsK978J7yxpLiBa0O1yu2Qsw6pEPMI5g%22,%22where%22:{%22ands%22:[{%22input%22:{%22type%22:%22netcachekey%22,%22idtype%22:%22cid%22,%22key%22:%22mN8__d9cuuCNzjLXsK978J7yxpLiBa0O1yu2Qsw6pEPMI5g%22}}]}}&showcolumndisplayname=1' -O ms-ms.csv
+# MS-MS (hnid=1857020)
+pubchem_download 1857020 "ms-ms.csv"
 divider
 
 # Merge all pubchem csvs
@@ -50,16 +62,16 @@ divider
 rm pubchem.csv firstblocks_pubchem.csv
 
 # Merge PubChem and PubChemLite
-csvstack "$TRIMMED" reordered_pubchem.csv > total.csv
+csvstack reordered_pubchem.csv "$TRIMMED" > total.csv
 rm "$TRIMMED" reordered_pubchem.csv
 
 # Remove duplicates
 go run ./csv_magic/dedupe/dedupe.go total.csv
-DATASET_NAME="cts-lite_$(date +%Y%m%d)".csv
+DATASET_NAME="${1:-cts-lite_$(date +%Y%m%d).csv}"
 mv deduped_total.csv "$DATASET_NAME"
 rm total.csv
 divider
 
 echo "Dataset '$DATASET_NAME' created successfully!"
-echo "Took  $(($(date +%s) - START_TIMER)) seconds"
+echo "Took $(($(date +%s) - START_TIMER)) seconds"
 
