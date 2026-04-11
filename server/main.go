50e23b444a6fd9b2363cc3ff711d21aae9c6f5b0
jvogel <joevogel501@gmail.com>
2026-04-10 17:28:44 -0700

    Allow POST methods for CORS

--- server/main.go
+++ server/main.go
@@ -14,7 +14,7 @@ func corsMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// Set CORS headers
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type")
 
 		// Handle preflight OPTIONS request
